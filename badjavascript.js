cad6691a32afb9addc8716468b35159468afabc6
adam00003456 <adam000034@gmail.com>
2019-03-18 22:29:09 -0700

    update
    update

--- badjavascript.js
+++ badjavascript.js
@@ -1,5 +1,14 @@
-function externalLibrary (date) {
-  throw new Error(`externalLibrary method broken: ${date}`);
+class brokenjavascript {
+  var date = ""
+  constructor(date) {
+    this.date=date;
+  }
+  
+  getDate () {
+    throw new Error(`externalLibrary method broken: ${date}`);
+  }
 }
+
+
 
 
